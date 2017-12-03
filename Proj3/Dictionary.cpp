eec647ae7543756ef0eeeee393af76cd0469f0e7
Kaizen Castanos <kaizenc98@gmail.com>
2017-12-03 16:38:45 -0500

    more work

--- Proj3/Dictionary.cpp
+++ Proj3/Dictionary.cpp
@@ -4,7 +4,12 @@ using namespace std;
 
 //------------Dictionary Class Definitions-----------//
 Dictionary::Dictionary(){
-    //
+    height = 0;
+    size = 0;
+    head -> nullptr;
+}
+int Dictionary::Size(){
+	return size;
 }
 
 int main(){
--- Proj3/Dictionary.h
+++ Proj3/Dictionary.h
@@ -1,20 +1,36 @@
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
 
+//Design this so we use a binary search tree, implement BST?
+template<class KeyType, class ItemType>
+class Node{
+    Node *left;
+    Node *right;
+    KeyType key;
+    ItemType item;
+};
+
 template<class KeyType, class ItemType>
 class Dictionary{
-    KeyType* keys;
-    ItemType* items;
+    Node *head;
     int height;
+    int size;
   public:
-    Dictionary();
+    Dictionary(); //Constructor
     bool IsEmpty(); //returns true if container is empty
     void Clear(); //removes all items and leaves container empty
     int Size(); //returns number of items
     int Height(); //returns height of BST
-    int Count(KeyType key); //returns number of items
-    ItemType operator[](KeyType key); //returns reference to item with key - should also create
+    int Count(KeyType key); //returns 1 if exists, 0 if otherwise, aka search
+    ItemType* operator[](KeyType key); //returns reference to item with key - should also create if DNE
     
     void Remove(KeyType key);
     void Balance();
 };
+/*
+IsEmpty is true if Node is a null pointer
+Clear recursively goes through the BST and deletes the pointers (go to the bottom, when bottom is done, do top)
+Size should just keep track of size
+Count is just a search, as is operator[]
+
+*/
