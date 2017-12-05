47bfea45aa9d92fe2b719b1ccab1582ecd271280
Kaizen Castanos <kaizenc98@gmail.com>
2017-12-05 17:41:32 -0500

    Work

--- Proj3/Dictionary.cpp
+++ Proj3/Dictionary.cpp
@@ -5,8 +5,41 @@ using namespace std;
 
 //---------------Node Class Definitions--------------//
 template<class KeyType, class ItemType>
-ItemType Node<KeyType,ItemType>::getItem(){
-	return item;
+Node<KeyType,ItemType>::Node(){
+	left = NULL;
+	right = NULL;
+}
+template<class KeyType, class ItemType>
+Node<KeyType,ItemType>::Node(KeyType new_key){
+	Node();
+	key = new_key;
+}
+template<class KeyType, class ItemType>
+Node<KeyType,ItemType>::Node(KeyType new_key, ItemType new_item){
+	Node();
+	key = new_key;
+	item = new_item;
+}
+
+template<class KeyType, class ItemType>
+ItemType * Node<KeyType,ItemType>::getItem(){
+	return &item;
+}
+template<class KeyType, class ItemType>
+KeyType Node<KeyType,ItemType>::getKey(){
+	return key;
+}
+template<class KeyType, class ItemType>
+Node<KeyType,ItemType> * Node<KeyType,ItemType>::getLeft(){
+	return left;
+}
+template<class KeyType, class ItemType>
+Node<KeyType,ItemType> * Node<KeyType,ItemType>::getRight(){
+	return right;
+}
+template<class KeyType, class ItemType>
+void Node<KeyType,ItemType>::operator=(ItemType new_item){
+	item = new_item;
 }
 
 //------------Dictionary Class Definitions-----------//
@@ -15,6 +48,13 @@ Dictionary<KeyType,ItemType>::Dictionary(){
     height = 0;
     size = 0;
     head = NULL;
+}
+
+template<class KeyType, class ItemType>
+Dictionary<KeyType,ItemType>::Dictionary(Node<KeyType,ItemType> * node){
+	height = 0;
+    size = 0;
+    head = node;
 }
 
 template<class KeyType, class ItemType>
@@ -46,15 +86,58 @@ int Dictionary<KeyType, ItemType>::Height_Helper(Node<KeyType,ItemType>* node){
 
 
 template<class KeyType, class ItemType>
-ItemType* Dictionary<KeyType, ItemType>::operator[](KeyType key){
-	return search(key)->getItem();
+ItemType & Dictionary<KeyType, ItemType>::operator[](KeyType key){
+	Node<KeyType,ItemType> * node = search(head, key);
+	if(node == NULL){
+		node = Node<KeyType,ItemType>(key);
+		insert(node);
+		return node;
+	}
+	return node;	
 }
 template<class KeyType, class ItemType>
-Node<KeyType,ItemType> * Dictionary<KeyType, ItemType>::search(KeyType key){
+Node<KeyType,ItemType> * Dictionary<KeyType, ItemType>::search(Node<KeyType,ItemType>* curr, KeyType key){
+	if(curr == NULL) return NULL;
+	Node<KeyType,ItemType> * left = NULL;
+	Node<KeyType,ItemType> * right = NULL;
+
+	if(key == curr->getKey()){
+		return curr;
+	}else if(key < curr->getKey()){
+		if(curr->getLeft() != NULL){
+			left = search(curr->getLeft(), key);
+		}
+	}else{
+		if(curr->getRight() != NULL){				
+			right = search(curr->getRight(), key);
+		}
+	}
+	if(left!=NULL) return left;
+	if(right!=NULL) return right;
 	return NULL;
+}
+template<class KeyType, class ItemType>
+void Dictionary<KeyType, ItemType>::insert(Node<KeyType,ItemType>* curr, Node<KeyType,ItemType>* node){
+	if(curr == NULL) curr = node;
+	if(node->getKey() <= curr->getKey()){
+		if(curr->getLeft() == NULL){
+			curr->getLeft() = node;
+		}else{
+			insert(curr->getLeft(), node);
+		}
+	}else{
+		if(curr->getRight() == NULL){
+			curr->getRight() = node;
+		}else{
+			insert(curr->getRight(), node);
+		}
+	}
 }
 
 //----------------Main Function Test---------------//
-int main(){
-    //
+int main(){    
+    Node<string, int> node("potato",3);    
+    Dictionary<string, int> container(&node);
+
+   int a = container["potato"];
 }
--- Proj3/Dictionary.h
+++ Proj3/Dictionary.h
@@ -8,9 +8,16 @@ class Node{
     KeyType key;
     ItemType item;
   public:
+    //Constructors
     Node();
+    Node(KeyType new_key);
+    Node(KeyType new_key, ItemType new_item);
+
     void operator=(ItemType item);
-    ItemType getItem();
+    ItemType * getItem();
+    KeyType getKey();
+    Node<KeyType,ItemType> * getLeft();
+    Node<KeyType,ItemType> * getRight();
 };
 
 template<class KeyType, class ItemType>
@@ -20,15 +27,19 @@ class Dictionary{
     int size;
 
     int Height_Helper(Node<KeyType,ItemType>* node);
-    Node<KeyType,ItemType> * search(KeyType key);
+    Node<KeyType,ItemType> * search(Node<KeyType,ItemType>* curr, KeyType key);
+    void insert(Node<KeyType,ItemType>* top, Node<KeyType,ItemType>* node); //recursive, node has to be placed below top
   public:
-    Dictionary(); //Constructor
+    //Constructors
+    Dictionary(); 
+    Dictionary(Node<KeyType,ItemType> * node);
+
     bool IsEmpty(); //returns true if container is empty
     void Clear(); //removes all items and leaves container empty
     int Size(); //returns number of items
     int Height(); //returns height of BST
     int Count(KeyType key); //returns 1 if exists, 0 if otherwise, aka search
-    ItemType* operator[](KeyType key); //returns reference to item with key - should also create if DNE
+    ItemType & operator[](KeyType key); //returns reference to item with key - should also create if DNE
     
     void Remove(KeyType key);
     void Balance();
