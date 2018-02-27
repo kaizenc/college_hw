cb8f4f731415961e506fb4258b183337bf8d4f3d
imgntn159 <imgtn159@gmail.com>
2018-02-27 02:47:40 +0000

    ayo what's good

--- 335Proj1/main.cpp
+++ 335Proj1/main.cpp
@@ -1,34 +1,45 @@
 #include <iostream>
+#include <stdexcept>
 #include <fstream>
 #include <vector>
 #include <string>
 #include "bitcoin.h"
 #include "pseudoserver.h"
 #include "tree.h"
+using namespace std;
 
 int main(int argc, char *argv[]){
-	//if argc=2, only input file supplied
-	//if argc=3, input/output supplied
-	//if argc=4, input/output and seed supplied
 	
-	if(argc < 2){
-		cerr << "Insufficient params" << endl;
+	if(argc < 4){
+		cerr << "Error: Insufficient Parameters" << endl;
 		exit(1);
 	}
-	string filename = "smol.csv";
-	ifstream tree_data(filename);
-	ofstream output("out2.txt");
-
+	string in_filename = argv[1];
+	ifstream tree_data(in_filename);
 	if (!tree_data) { //If file fails to open
-	  	cerr << "Unable to open file " << filename << endl;
+	  	cerr << "Unable to open input file: " << in_filename << endl;
 	    exit(1);
 	}
-
+	string out_filename = argv[2];
+	ofstream output(out_filename);
+	if (!output.is_open()) { //If file fails to open
+	  	cerr << "Unable to open output file: " << out_filename << endl;
+	    exit(1);
+	}
+	
 	int seed = 0;
+	try{
+		seed = stoi(argv[3]);
+	}catch(const invalid_argument& ia){
+		cerr << "Last parameter isn't an integer: " << argv[3] << endl;
+		exit(1);
+	}
+	
 	int num_of_flips = 0;
 	int q_size_sum = 0;
 	int empty_q_count = 0;
 	int eof_q_size = 0;
+	int max_q_size = 0;
 
 	Bitcoin coin = Bitcoin(seed);
 	PseudoServer server;
@@ -39,14 +50,23 @@ int main(int argc, char *argv[]){
 		if(coin() == 0){
 			string line = "";
 			if(server.extract(line)){
-				cout << line << endl << endl;
 				Tree new_tree(line);
 				output << new_tree;
 			}
 		}else{//coin is 1
 			if(!server.read(tree_data)){
 				server_is_done = true;
+				eof_q_size = server.queuesize();
 			}
+		}
+		//keep track of stats
+		num_of_flips++;
+		q_size_sum += server.queuesize();
+		if(server.queuesize() == 0){
+			empty_q_count++;
+		}
+		if(server.queuesize() > max_q_size){
+			max_q_size = server.queuesize();
 		}
 	}
 	
@@ -55,5 +75,13 @@ int main(int argc, char *argv[]){
 		Tree new_tree(line);
 		output << new_tree;
 	}
+	tree_data.close();
+	output.close();
+	cout << "average queue size: " << (q_size_sum*1.0)/num_of_flips << endl;
+	cout << "maximum queue size: " << max_q_size << endl;
+	cout << "empty queue count: " << empty_q_count << endl;
+	cout << "queue size on eof: " << eof_q_size << endl;
+	
+	
 	
 }
