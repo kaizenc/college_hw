6c7fd33e4976febc31c23116da4a901a06bab5ae
Kaizen Castanos <kaizenc98@gmail.com>
2018-05-13 23:22:25 -0400

    mneh mneh

--- 335Proj3/subway_station.h
+++ 335Proj3/subway_station.h
@@ -15,50 +15,21 @@
 #define __SUBWAY_STATION_H__
 #include <iostream>
 #include <string>
+#include <vector>
+#include "subway_entrance.h"
 using namespace std;
 
-//simply unpack everything into variables
+//holds a vector of entrances and the centroid location
 class subway_station{
 private:
-	//sample: 51,http://web.mta.info/nyct/service/,Parsons Blvd & Archer Ave at NE corner, POINT (-73.799784000399 40.70235300071414) ,E-J-Z
-	int ObjectID;
-	string URL;
-	string name;
 	double longitude;
 	double latitude;
-	vector<string> arr_lines;
-	unsigned long line_mask;
-	void convertToMask(string lines);
-	int line_hash(string x);
+	vector<subway_entrance> entrances;
 public:
-	//Subway Entrance Constructor
-	subway_station(const string & data);
-
-	/** getLong(): returns the longitude of this subway entrance
-		* @return double 	The longitude of this subway entrance
-		*/
+	void insert_entrance();
+	vector<subway_entrance> getEntrances();
 	double getLong();
-
-	/** getLat(): returns the latitude of this subway entrance
-		* @return double 	The latitude of this subway entrance
-		*/
 	double getLat();
-
-	/** getLat(): returns the name of this subway entrance
-		* @return std::string 	The name of this subway entrance
-		*/
-	string getName();
-
-	/** getMask(): returns a long to be used as a bitmask, formed
-		* 	using a hash function for the lines
-		* @return long 	A bitmask representing the lines of this entrance
-		*/
-	long getMask();
-
-	/** getLines(): returns a vector containing the lines of this entrance
-		* @return vector<std::string> 	A vector containing the lines of this entrance
-		*/
-	vector<string> getLines();
 };
 
 
